ad62eac2b7029e42466119ab31e2d048fbf3fffc
alstep07 <sashastepanenko2007@gmail.com>
2025-04-25 18:07:15 +0300

    fix: header layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import { ConnectWallet } from "@/shared/components/connect-wallet";
+import { WandIcon } from "lucide-react";
 
 export default function Home() {
   return (
     <div className="min-h-screen p-8 flex flex-col items-center gap-6">
-      <h1 className="text-2xl font-bold">WalletWizard</h1>
+      <div className="flex items-center gap-2">
+        <WandIcon className="size-8 text-violet-800" />
+        <h1 className="text-2xl font-bold">Wallet Wizard </h1>
+      </div>
       <ConnectWallet />
     </div>
   );
